ecd7ac2e6efdb185406672119eaabb200abda141
Plot <99035346+Finaplot@users.noreply.github.com>
2022-10-12 22:09:08 +0800

    粘液科技附属【无尽科技汉化版改版】
    模拟器：初始消耗电力由300改至1000
    凋零模拟器：删除下届之心生成，经验生成由32改至64，电力消耗由9000改至12000
    末影龙模拟器：龙蛋生成概率由百万分之一调整至千分之一，经验生成由96改至128，电力消耗由18000改至24000，产物虚空粉更改为虚空粒，
     产物新增龙之心，概率调整：概率由0.79，0.19，7.99*10^-7，变为0.79，0.19，1.596*10^-3，7.98*10^-4
    奇点：所有原版矿物奇点输入数量统一更改为3000，新增虚空奇点，无尽奇点和虚空奇点输入数量均为600
    龙之心：可由末影龙模拟器获得，也可以通过构造器消耗一组龙蛋获得 
    龙之心拆解器:电力消耗1wj/s，龙之心可以拆卸为除了无尽的所有奇点，其中矿物奇点1:4，虚空奇点1:2
    无尽套+无尽盾+无尽工具+无尽武器+无尽飞行器：属性以及配方修改
    生物模拟器输出产物以及经验时间由10s改为20s
    更改了武器装备的属性以及加成

---
+++ src/main/java/io/github/mooy1/infinityexpansion/items/gear/InfinityMatrix.java
@@ -0,0 +1,96 @@
+package io.github.mooy1.infinityexpansion.items.gear;
+
+import java.util.Iterator;
+import java.util.List;
+
+import javax.annotation.Nonnull;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import io.github.mooy1.infinitylib.common.Events;
+import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
+import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
+import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
+import io.github.thebusybiscuit.slimefun4.core.attributes.NotPlaceable;
+import io.github.thebusybiscuit.slimefun4.core.attributes.Soulbound;
+import io.github.thebusybiscuit.slimefun4.core.handlers.ItemUseHandler;
+import io.github.thebusybiscuit.slimefun4.implementation.items.SimpleSlimefunItem;
+
+public final class InfinityMatrix extends SimpleSlimefunItem<ItemUseHandler> implements Listener, Soulbound, NotPlaceable {
+
+    public InfinityMatrix(ItemGroup category, SlimefunItemStack item, RecipeType type, ItemStack[] recipe) {
+        super(category, item, type, recipe);
+        Events.registerListener(this);
+    }
+
+    private static void disableFlight(Player p) {
+        p.sendMessage(ChatColor.RED + "无尽飞行已禁用!");
+        p.setAllowFlight(false);
+    }
+
+    private static void enableFlight(Player p) {
+        p.sendMessage(ChatColor.GREEN + "无尽飞行已启用!");
+        p.setAllowFlight(true);
+    }
+
+    @Nonnull
+    @Override
+    public ItemUseHandler getItemHandler() {
+        return e -> {
+            ItemStack item = e.getItem();
+            if (!item.hasItemMeta()) {
+                return;
+            }
+            ItemMeta meta = item.getItemMeta();
+            if (!meta.hasLore()) {
+                return;
+            }
+            List<String> lore = meta.getLore();
+
+            Player p = e.getPlayer();
+
+            Iterator<String> iterator = lore.listIterator();
+
+            while (iterator.hasNext()) {
+                String line = iterator.next();
+
+                if (ChatColor.stripColor(line).contains("UUID: ")) {
+                    String uuid = ChatColor.stripColor(line).substring(6);
+
+                    if (!p.getUniqueId().toString().equals(uuid)) {
+                        p.sendMessage(ChatColor.YELLOW + "你不是飞行器的主人!");
+                        return;
+                    }
+
+                    if (p.isSneaking()) { //remove owner
+                        iterator.remove();
+                        meta.setLore(lore);
+                        item.setItemMeta(meta);
+                        p.sendMessage(ChatColor.GOLD + "已解除绑定飞行器!");
+                        disableFlight(p);
+
+                    }
+                    else if (p.getAllowFlight()) {
+                        disableFlight(p);
+                    }
+                    else {
+                        enableFlight(p);
+                    }
+
+                    return;
+                }
+            }
+
+            lore.add(ChatColor.GREEN + "UUID: " + p.getUniqueId());
+            meta.setLore(lore);
+            item.setItemMeta(meta);
+            p.sendMessage(ChatColor.GOLD + "已绑定飞行器!");
+            enableFlight(p);
+        };
+    }
+
+}
